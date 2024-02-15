86eff28b31c6071208b71aea18a57a18ecfc9845
rania chakroun <rania.chakroun@etudiant-enit.utm.tn>
2024-02-15 14:07:37 +0100

    adding some ngIf directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username= '' ;
+  deleteMessage: string = '';
+  deleteUsername() {
+    this.deleteMessage = `Username '${this.username}' was deleted.`;
+    this.username = '';
+  }
   }
